04a5d98f671c1ec008bc543c337ebb8197f54947
EDPC\Edward <edwardngo92@gmail.com>
2018-10-17 21:51:42 +1100

    added UI-menu navigation buttons, can now switch between scenes

---
+++ Fentown1/Assets/Scripts/_Game/MenuScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+    [SerializeField] private Object GameScene;
+
+    private SceneController sceneController;
+    private bool inMainMenu;
+
+	void Start () {
+
+        inMainMenu = false;
+	}
+	
+	void Update () {
+
+        if (!inMainMenu) {
+            inMainMenu = true;
+        }
+	}
+
+    public void StartGame() {
+        inMainMenu = false;
+        SceneManager.LoadScene("GameScene");
+    }
+}
---
+++ Fentown1/Assets/Scripts/_Game/SceneController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+    public Scene currentScene;
+    public string currentSceneName;
+
+    public bool isGameActive = false;
+
+    // Use this for initialization
+    void Start () {
+        DontDestroyOnLoad(this.gameObject);
+        currentScene = SceneManager.GetActiveScene();
+        currentSceneName = currentScene.name;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (currentScene != SceneManager.GetActiveScene()) {
+            currentScene = SceneManager.GetActiveScene();
+            currentSceneName = currentScene.name;
+        }
+	}
+
+
+    public void SetGameScene() {
+        SceneManager.LoadScene("GameScene" , LoadSceneMode.Single);
+    }
+
+    public void OptionsMenu() {
+        SceneManager.LoadScene("GameScene" , LoadSceneMode.Single);
+    }
+
+    public void SetMainMenuScene() {
+        SceneManager.LoadScene("MainMenuScene" , LoadSceneMode.Single);
+    }
+
+
+    public void ExitGame() {
+        // CLOSE APPLICATION!
+    }
+}
