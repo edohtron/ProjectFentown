7b1b85abce89da4768b68dfe57791084fa59e60b
EDPC\Edward <edwardngo92@gmail.com>
2018-10-17 22:50:36 +1100

    INITIAL COMMIT #2

---
+++ Fentown1.1/Assets/_Scripts/PauseMenuScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuScript : MonoBehaviour {
+
+    private SceneControllerScript _sceneController;
+    private GameObject pauseMenuCanvas;
+
+	// Use this for initialization
+	void Start () {
+        _sceneController = GameObject.Find("SCENECONTROLLER").GetComponent<SceneControllerScript>();
+        pauseMenuCanvas = gameObject.transform.Find("PauseMenu").gameObject;
+        pauseMenuCanvas.SetActive(false);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!_sceneController) {
+            _sceneController = GameObject.Find("SCENECONTROLLER").GetComponent<SceneControllerScript>();
+        }
+
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (!pauseMenuCanvas.activeInHierarchy) {
+                pauseMenuCanvas.SetActive(true);
+            }
+
+            else {
+                pauseMenuCanvas.SetActive(false);
+            }
+        }
+       
+	}
+
+
+}
---
+++ Fentown1.1/Assets/_Scripts/SceneControllerScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControllerScript : MonoBehaviour {
+
+    private Scene currentScene;
+    public string currentSceneName;
+
+    public bool isGameActive = false;
+
+    private void Awake() {
+        DontDestroyOnLoad(this.gameObject);
+
+        if (FindObjectsOfType(GetType()).Length > 1) {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void Start() {
+        currentScene = SceneManager.GetActiveScene();
+        currentSceneName = currentScene.name;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (currentScene != SceneManager.GetActiveScene()) {
+            currentScene = SceneManager.GetActiveScene();
+            currentSceneName = currentScene.name;
+        }
+    }
+
+
+    public void SetGameScene() {
+        SceneManager.LoadScene("01_GameScene" , LoadSceneMode.Single);
+    }
+
+    public void OptionsMenu() {
+        SceneManager.LoadScene("01_GameScene" , LoadSceneMode.Single);
+    }
+
+    public void SetMainMenuScene() {
+        SceneManager.LoadScene("00_MainMenuScene" , LoadSceneMode.Single);
+    }
+
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+}
